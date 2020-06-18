47b0189a6f27aec675bc42b0a95c9586a09edf8a
Adam Workman <adammworkman25@gmail.com>
2020-06-18 18:50:08 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,5 +9,8 @@ birthday_kids.each do |kids_name, age|
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
   end
-def 
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+        if age < 12 
+
 
